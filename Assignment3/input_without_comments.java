f73ad51246496af6e262a3ed0f320bc479e0a5c0
samyakjain0657 <jainsamyak554@gmail.com>
2019-03-11 19:07:42 +0530

    object def done

--- Assignment3/input_without_comments.java
+++ Assignment3/input_without_comments.java
@@ -1,110 +1,102 @@
-public class RecursionExampleDirectory
-{	
-	public int getSize(Directory dir)
-	{	int total = 0;
-	
-		 
-		File[] files = dir.getFiles();
-		for(int i = 0; i < files.length; i++)
-			total += files[i].getSize();
-			
-		 
-		Directory[] subs = dir.getSubs();
-		for(int i = 0; i < subs.length; i++)
-			total += getSize(subs[i]);
-			
-		return total;
-	}
-	
-	public static void main(String[] args)
-	{	RecursionExampleDirectory r = new RecursionExampleDirectory();
-		Directory d = new Directory();
-		System.out.println( r.getSize(d) );
-	}
-	
-	 
-	public static int fact(int n)
-	{	int result = 0;
-		if(n == 0)
-			result = 1;
-		else
-			result = n * fact(n-1);
-		return result;
-	}
-	
-	 
-	public static int pow(int base, int exp)
-	{	int result = 0;
-		if(exp == 0)
-			result = 1;
-		else
-			result = base * pow(base, exp - 1);
-		return result;
-	}
-	
-	 
-	 
-	public static int fib(int n)
-	{	int result = 0;
-		if(n == 1 || n == 2)
-			result = 1;
-		else
-			result = fib(n-1) + fib(n-2);
-		return result;
-	}
-	
-	public static int minWasted(int[] items, int itemNum, int capLeft)
-	{	int result = 0;
-		if(itemNum >= items.length)
-			result = capLeft;
-		else if( capLeft == 0)
-			result = 0;
-		else
-		{	int minWithout = minWasted(items, itemNum + 1, capLeft);
-			if( capLeft <= items[itemNum])		
-			{	int minWith = minWasted(items, itemNum + 1, capLeft - items[itemNum]);
-				result = Math.min(minWith, minWithout);
-			}
-			else
-				result = minWithout;
-		}
-		return result;	
-	}
+ 
+ 
+ 
+ 
+ 
+ 
+public class DemoClass
+{
+    private int x;
+
+    public DemoClass()
+    {
+         
+        x = 0;
+    }
+
+    public DemoClass(int x)
+    {
+         
+         
+         
+        this.x = x;
+    }
+
+    public DemoClass(DemoClass otherDemo)
+    {
+         
+        this.x = otherDemo.x;
+    }
+
+     
+    public static void s1() {
+        return;
+    }
+     
+    public void i1() {
+        return;
+    }
+
+     
+     
+    public static void s2() {
+ 
+        s1();        
+        return;
+    }
+
+     
+     
+    public void i2() {
+        s1();        
+        i1();        
+        return;
+    }
+
+     
+     
+    public void overloadTester() {
+        System.out.println("");
+
+        overload((byte)1);
+        overload((short)1);
+        overload(1);
+        overload(1L);
+        overload(1.0f);
+        overload(1.0);
+        overload('');
+        overload(true);
+    }
+    
+    public void overload(byte b) {
+        System.out.println("");
+    }    
+    public void overload(short s) {
+        System.out.println("");
+    }    
+    public void overload(int i) {
+        System.out.println("");
+    }
+    public void overload(long l) {
+        System.out.println("");
+    }
+    public void overload(float f) {
+        System.out.println("");
+    }
+    public void overload(double d) {
+        System.out.println("");
+    }    
+    public void overload(char c) {
+        System.out.println("");
+    }    
+    public void overload(boolean b) {
+        System.out.println("");
+    }    
+
+    public static void main(String[] args) {
+        DemoClass dc = new DemoClass();
+        dc.overloadTester();
+    }
 }
 
-class Directory
-{	private Directory[] mySubs;
-	private File[] myFiles;
-	
-	public Directory()
-	{	int numSubs = (int)(Math.random() * 3);
-		mySubs = new Directory[numSubs];
-		int numFiles = (int)(Math.random() * 10);
-		myFiles = new File[numFiles];
-		
-		for(int i = 0; i < myFiles.length; i++)
-			myFiles[i] = new File( (int)(Math.random() * 1000 ) );
-		for(int i = 0; i < mySubs.length; i++)
-			mySubs[i] = new Directory();
-	}
-	
-	public Directory[] getSubs()
-	{	return mySubs;
-	}
-	
-	public File[] getFiles()
-	{	return myFiles;
-	}
-}
-
-class File
-{	private int iMySize;
-
-	public File(int size)
-        {	iMySize = size;
-	}
-	
-	public int getSize()
-	{	return iMySize;
-	}
-}
+ 
